882491d88a287bcdd69dfef7ad69cba96849c701
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-03 21:05:43 +0500

    Task1

---
+++ Tyuiu.FedorovaDA.Sprint3.Task1.V6.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task1.V6.Lib
+{
+    public class DataService : ISprint3Task1V6
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            int a = 4;
+            
+            startValue = 1;
+            stopValue = 10;
+            double res = 1;
+            while (startValue <= stopValue)
+            {
+                res += (Math.Pow(a, startValue) + 1) * Math.Sin(startValue);
+                startValue++;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.FedorovaDA.Sprint3.Task1.V6/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.FedorovaDA.Sprint3.Task1.V6.Lib;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task1.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for,                                  *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 1;
+            int stopValue = 10;
+            double res = 1;
+            int a = 4;
+
+            Console.WriteLine("Значение К =" + startValue);
+            Console.WriteLine("Количество периода = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            res = ds.GetSumSeries(a, startValue, stopValue);
+            Console.WriteLine("Значение выражения = " + res);
+
+        }
+    }
+}
