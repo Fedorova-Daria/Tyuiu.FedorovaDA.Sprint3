a61720ab55e41a867047cdc7dd9a58a0c490478a
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-04 19:37:03 +0500

    Task3

---
+++ Tyuiu.FedorovaDA.Sprint3.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task2.V2.Lib
+{
+    public class DataService : ISprint3Task2V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double res = 1;
+            int k = 1;
+            stopValue = 7;
+            do
+            {
+                res *= Math.Sin(0.1) + k;
+                k++;
+            }while(k <= stopValue);
+            return Math.Round(res , 3);
+        }
+    }
+}
---
+++ Tyuiu.FedorovaDA.Sprint3.Task2.V2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.FedorovaDA.Sprint3.Task2.V2.Lib;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task2.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while,                                *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при a=4.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int k = 1;
+            int stopValue = 7;
+
+            Console.WriteLine("Значение К =" + k);
+            Console.WriteLine("Количество периода = " + stopValue);
+
+            double res = ds.GetMultiplySeries(k, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение выражения = " + res);
+        }
+    }
+}
