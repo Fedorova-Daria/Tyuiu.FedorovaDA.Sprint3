5e2ef9bfad63a46eae20b9eec7098dcf6c58c8a6
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-04 22:35:40 +0500

    Task4

---
+++ Tyuiu.FedorovaDA.Sprint3.Task4.V23/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.FedorovaDA.Sprint3.Task4.V23.Lib;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y=(cos(x)/x)+3 При х = 0 прервать цикл.      *");
+            Console.WriteLine("* Полученные значения перемножать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага: " + startValue);
+            Console.WriteLine("Конец шага: " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда равно " + ds.Calculate(startValue, stopValue));
+        }
+    }
+}
