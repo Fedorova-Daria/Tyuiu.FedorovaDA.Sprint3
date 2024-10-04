61d753528a3f9edfa79820f37cbbad2615d9691e
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-04 23:14:04 +0500

    Task5

---
+++ Tyuiu.FedorovaDA.Sprint3.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.FedorovaDA.Sprint3.Task5.V1.Lib
+{
+    public class DataService : ISprint3Task5V1
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeries += 1 / (Math.Cos(j) + x);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
